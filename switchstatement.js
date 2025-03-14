77767f6b9a2be6f85dc86e7b7174b775512cbb89
ramranjanjha89 <ramranjanjha89.rr@gmail.com>
2025-03-14 13:58:09 -0700

    java scrript switch statement code

---
+++ switchstatement.js
@@ -0,0 +1,26 @@
+console.log("----------switch stament case and discussion----------");
+
+let days=4;
+
+
+switch(days)
+{
+case 1:
+console.log(`today is monday ${days}`);
+break;
+case 2:
+case 3:
+case 4:
+console.log(`today is thrusday and badmint playday ${days}`);
+break;
+case 5:
+console.log(`today is friday ${days}`);
+break;
+case 6:
+console.log(`today is saturday ${days}`);
+break;
+case 7:
+console.log(`today is sunday ${days}`);
+break;
+
+}
