726232c887cbcefeae511b38d8e33bae37987a43
ramranjanjha89 <ramranjanjha89.rr@gmail.com>
2025-03-19 03:53:01 +0530

    arrow function code push

---
+++ arrowfunction.js
@@ -0,0 +1,17 @@
+console.log("---------------- arrow function-------------------\n");
+
+//wokring with anonymous function include arroe function:
+
+let sum= (num1,num2)=>
+{
+return num1+num2;
+}
+console.log(sum(10,20));
+//another way to write if there will be one statement:
+
+let multi=(num3,num4)=>
+num3*num4;
+//now just access and print the logic and call function:
+
+console.log(multi(3,4)); //output:12 
+
